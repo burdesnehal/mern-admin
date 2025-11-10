b29e6ff045449417d17839135846232272152414
Snehal Ravindra Burde <130461570+burdesnehal@users.noreply.github.com>
2025-11-10 14:21:50 +0530

    Simplify PublicRoute component
    Removed authentication check and animation from PublicRoute.

--- frontend/src/router/PublicRoute.jsx
+++ frontend/src/router/PublicRoute.jsx
@@ -1,27 +1,11 @@
 import React from "react";
-import { motion } from "framer-motion";
-import { Route, Redirect } from "react-router-dom";
-import * as authService from "@/auth";
+import { Route } from "react-router-dom";
 
 const PublicRoute = ({ component: Component, ...rest }) => {
   return (
-    // Show the component only when the admin is logged in
-    // Otherwise, redirect the admin to /signin page
     <Route
       {...rest}
-      render={(props) =>
-        authService.token.get() ? (
-          <Redirect to="/" />
-        ) : (
-          <motion.div
-            initial={{ x: 200 }}
-            animate={{ x: 0 }}
-            exit={{ scale: 0 }}
-          >
-            <Component {...props} />
-          </motion.div>
-        )
-      }
+      render={(props) => <Component {...props} />}
     />
   );
 };
