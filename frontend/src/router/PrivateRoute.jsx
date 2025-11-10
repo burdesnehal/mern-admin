21614e5c409761075905acdd878f4771ea8af7bc
Snehal Ravindra Burde <130461570+burdesnehal@users.noreply.github.com>
2025-11-10 14:21:22 +0530

    Simplify PrivateRoute component implementation

--- frontend/src/router/PrivateRoute.jsx
+++ frontend/src/router/PrivateRoute.jsx
@@ -1,35 +1,11 @@
 import React from "react";
-import { motion } from "framer-motion";
-import { Route, Redirect } from "react-router-dom";
-
-import * as authService from "@/auth";
+import { Route } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const config = {
-    type: "spring",
-    damping: 20,
-    stiffness: 100,
-  };
   return (
-    // Show the component only when the admin is logged in
-    // Otherwise, redirect the admin to /signin page
-
     <Route
       {...rest}
-      render={(props) =>
-        authService.token.get() ? (
-          <motion.div
-            transition={config}
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -20, opacity: 0 }}
-          >
-            <Component {...props} />
-          </motion.div>
-        ) : (
-          <Redirect to="/login" />
-        )
-      }
+      render={(props) => <Component {...props} />}
     />
   );
 };
