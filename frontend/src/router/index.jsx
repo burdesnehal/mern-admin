0a366a9049307778043c8a5642c20c1e77c5e8f6
Snehal Ravindra Burde <130461570+burdesnehal@users.noreply.github.com>
2025-11-10 14:27:55 +0530

    Refactor Router to use react-router and redux

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import AppRouter from "./AppRouter";
-import { Layout } from "antd";
-import Navigation from "@/layout/Navigation";
+import { Router as RouterHistory } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import AppRouter from "./AppRouter"; // force load AppRouter always
+import history from "@/utils/history";
+import store from "@/redux/store";
 
 export default function Router() {
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Navigation />
-      <Layout style={{ minHeight: "100vh" }}>
+    <RouterHistory history={history}>
+      <Provider store={store}>
         <AppRouter />
-      </Layout>
-    </Layout>
+      </Provider>
+    </RouterHistory>
   );
 }
