c2ea6aade4d0424d81d55e54102a0a7590958d19
Snehal Ravindra Burde <130461570+burdesnehal@users.noreply.github.com>
2025-11-10 14:23:00 +0530

    Refactor AuthRouter to simplify routing logic

--- frontend/src/router/AuthRouter.jsx
+++ frontend/src/router/AuthRouter.jsx
@@ -1,7 +1,6 @@
 import React, { lazy, Suspense } from "react";
-import { Redirect, Route, Switch, useLocation } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import PublicRoute from "./PublicRoute";
 import PageLoader from "@/components/PageLoader";
 
 const Login = lazy(() =>
@@ -18,17 +17,9 @@ export default function AuthRouter() {
     <Suspense fallback={<PageLoader />}>
       <AnimatePresence exitBeforeEnter initial={false}>
         <Switch location={location} key={location.pathname}>
-          <PublicRoute
-            path="/"
-            component={Login}
-            render={() => <Redirect to="/login" />}
-          />
-          <PublicRoute component={Login} path="/login" exact />
-          <Route
-            path="*"
-            component={NotFound}
-            render={() => <Redirect to="/notfound" />}
-          />
+          <Route path="/" component={Login} exact />
+          <Route path="/login" component={Login} exact />
+          <Route path="*" component={NotFound} />
         </Switch>
       </AnimatePresence>
     </Suspense>
